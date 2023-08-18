3aa7be33270002fd7b5a1a5000adb2d19a8fd67e
jb <101614021+jbc2313@users.noreply.github.com>
2023-08-18 04:51:43 -0500

    UPDATE
    Helloworld.cc is finished.

--- helloworld.cc
+++ helloworld.cc
@@ -1,1 +1,26 @@
 #include "helloworld.h"
+#include <iostream>
+
+HelloWorld::HelloWorld() : m_button("Hello World")
+{
+    //Sets the margin around the button
+    m_button.set_margin(10);
+
+    // when the button is clicked(signaled), it will call the on_button_clicked()
+    m_button.signal_clicked().connect(sigc::mem_fun(*this,
+                &HelloWorld::on_button_clicked));
+
+    //This packs the button into the window (a container)
+    set_child(m_button);
+};
+
+HelloWorld::~HelloWorld()
+{
+
+
+};
+
+void HelloWorld::on_button_clicked()
+{
+    std::cout << "Hello World!" << std::endl;
+};
