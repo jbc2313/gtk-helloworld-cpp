f040aa4b2028e7efe85ab8ac3da52c91a0848332
jb <101614021+jbc2313@users.noreply.github.com>
2023-08-21 23:56:01 -0500

    added more buttons, and compile_commands.json to cmake

--- src/helloworld.cxx
+++ src/helloworld.cxx
@@ -1,17 +1,33 @@
 #include "helloworld.h"
 #include <iostream>
 
-HelloWorld::HelloWorld() : m_button("Hello World")
+HelloWorld::HelloWorld() 
+: m_button_1("Hello World"),
+  m_button_2("Gentoo Linux"),
+  m_button_quit("Quit")
 {
+    set_title("Gtk::Grid"); 
+
+    m_grid.set_margin(12);
+    set_child(m_grid);
+
+    m_grid.attach(m_button_1, 0,0);
+    m_grid.attach(m_button_2, 1,0);
+    m_grid.attach_next_to(m_button_quit, m_button_1, Gtk::PositionType::BOTTOM, 2, 1);
+
+
     //Sets the margin around the button
-    m_button.set_margin(10);
+    //m_button.set_margin(10);
 
     // when the button is clicked(signaled), it will call the on_button_clicked()
-    m_button.signal_clicked().connect(sigc::mem_fun(*this,
-                &HelloWorld::on_button_clicked));
+    m_button_1.signal_clicked().connect(sigc::bind(sigc::mem_fun(*this,&HelloWorld::on_button_clicked), "Hello World"));
 
+    m_button_2.signal_clicked().connect(sigc::bind(sigc::mem_fun(*this,&HelloWorld::on_button_clicked), "Gentoo Linux"));
+   
+    m_button_quit.signal_clicked().connect(sigc::mem_fun(*this, &HelloWorld::on_button_quit));
+    
     //This packs the button into the window (a container)
-    set_child(m_button);
+    //set_child(m_button);
 };
 
 HelloWorld::~HelloWorld()
@@ -20,7 +36,12 @@ HelloWorld::~HelloWorld()
 
 };
 
-void HelloWorld::on_button_clicked()
+void HelloWorld::on_button_quit()
 {
-    std::cout << "Hello World!" << std::endl;
+    hide();
+};
+
+void HelloWorld::on_button_clicked(const Glib::ustring& data)
+{
+    std::cout << data << std::endl;
 };
--- src/helloworld.h
+++ src/helloworld.h
@@ -3,20 +3,26 @@
 
 #include <gtkmm/button.h>
 #include <gtkmm/window.h>
+#include <gtkmm/grid.h>
 
 class HelloWorld : public Gtk::Window
 {
 
     public:
       HelloWorld();
-      ~HelloWorld() override;
+      //~HelloWorld() override; // original way shown
+      virtual ~HelloWorld(); // this is from later in gtkmm doc
 
     protected:
       //Signal handlers:
-      void on_button_clicked();
+    
+      //void on_button_clicked(const Glib::ustring& data); // single button
+      void on_button_clicked(const Glib::ustring& data);
+      void on_button_quit();
 
       //Member widgets:
-      Gtk::Button m_button;
+      Gtk::Grid m_grid;
+      Gtk::Button m_button_1, m_button_2, m_button_quit;
 };
 
 #endif // GTKMM_EXAMPLE_HELLOWORLD_H
